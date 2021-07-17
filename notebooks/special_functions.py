7a609403a67c81b4c40c73d26dadd202288748f2
danna01 <dannajaimes01@outlook.com>
2021-07-17 17:57:35 -0500

    Arreglo de plots de Materia oscura

--- notebooks/special_functions.py
+++ notebooks/special_functions.py
@@ -1,4 +1,4 @@
-def compile_data(origin,gal,minsnap,maxsnap,stepsnap):
+def compile_data(origin,gal,minsnap,maxsnap,stepsnap,limites1,limites2):
     """
     La funcion compile_data permite almacenar de manera
     eficiente la informacion respecto a la velocidad maxima, 
@@ -17,6 +17,12 @@ def compile_data(origin,gal,minsnap,maxsnap,stepsnap):
        numero maximo del snap considerado
     stepsnap : float
        valores de intervalo entre los snaps
+    limites1 : dict
+       valores para los limites considerados en el 
+       diccionario para cada galaxia (Gas)
+    limites2 : dict
+       valores para los limites considerados en el 
+       diccionario para cada galaxia (Star)
         
     Returns
     -------
@@ -41,12 +47,12 @@ def compile_data(origin,gal,minsnap,maxsnap,stepsnap):
         dicti[snap]['Nstar'] = N_star
 
         histo,bins,dens = fn.density(data.gas['pos'],data.gas['mass'],n=20,limits=limites_densidad[gal])
-        m,b = fn.fit_exponential(bins,dens,limits=limites_gas[gal])
+        m,b = fn.fit_exponential(bins,dens,limits=limites1)
         fn.plot_exponential(bins,dens,m,b,filename='plots_nuevos/'+gal+'/'+gal+'_gas_'+snap+'.jpg')
         dicti[snap]['h_gas'] = -1/m*np.log10(np.e)
         
         histo,bins,dens = fn.density(data.star['pos'],data.star['mass'],n=20,limits=limites_densidad[gal])
-        m,b = fn.fit_exponential(bins,dens,limits=limites_star[gal])
+        m,b = fn.fit_exponential(bins,dens,limits=limites2)
         fn.plot_exponential(bins,dens,m,b,filename='plots_nuevos/'+gal+'/'+gal+'_star_'+snap+'.jpg')
         dicti[snap]['h_star'] = -1/m*np.log10(np.e)
 
@@ -103,36 +109,25 @@ def show_results(dicti, dicti_old, gal):
     N_star_new = extract_qtt(dicti,'Nstar')
     Vmax_new = extract_qtt(dicti,'Vmax')
     
-    # Aca hacemos lo mismo para los datos viejos:
     snapshots_old = extract_qtt(dicti_old,'snapshots')
     h0_gas_old = extract_qtt(dicti_old,'h_gas')
     h0_star_old = extract_qtt(dicti_old,'h_star')
     N_star_old = extract_qtt(dicti_old,'Nstar')
     Vmax_old = extract_qtt(dicti_old,'Vmax')
     
-    # Definimos el tamaño del plot
     plt.figure(figsize=(12,4))
     
-    # Definimos cada una de las posiciones de los plots, los histogramas y las posiciones que les vamos a dar,
-    # asi como sus especificaciones para cada una de las variables.
-    # Para los valores de Y en el plot hemos asignado valores arbitrarios, con el unico fin de poder visualizar 
-    # la informacion.
-    
-    # Para el histograma del gas
     plt.subplot(131)
     plt.hist(h0_gas_new,bins=5,color='green')
     plt.title(gal + '  h0 GAS')
-    #Como solo existen datos viejos para las galaxias G0 y G1 debemos especificar que los plots son solo
-    #para esos datos con las siguientes lineas
+   
     if gal in ['G0','G1']:
         x = h0_gas_old
         y = np.array([50,50,50])
         plt.plot(x,y, "ok")
     
-    # Para el histograma de las estrellas
     plt.subplot(132)
-    #Como solo existen datos viejos para las galaxias G0 y G1 debemos especificar que los plots son solo
-    #para esos datos con las siguientes lineas
+  
     if gal in ['G0']:
         plt.hist(h0_star_new,bins=5,range=(0,4),color='orange')
         plt.title(gal + 'h0 STARS')
@@ -148,12 +143,91 @@ def show_results(dicti, dicti_old, gal):
         y = np.array([50,50,50])
         plt.plot(x,y,"ok")
     
-    #Para la velocidad maxima
+    
     plt.subplot(133)
     plt.hist(Vmax_new,bins=5,color='red')
     plt.title(gal + '  Max Velocity')
-    #Como solo existen datos viejos para las galaxias G0 y G1 debemos especificar que los plots son solo
-    #para esos datos con las siguientes lineas
+    
+    if gal in ['G0','G1']:
+        x = Vmax_old
+        y = np.array([50,50,50])
+        plt.plot(x,y,"ok")
+        
+
+def show_results_2(dicti, dicti_old, gal):
+    """
+    La funcion show_results te permite visualizar la informacion
+    y generar los histogramas con los datos de las propiedades
+    que calculados anteriormente, con las especificaciones necesarias.
+    
+    Parameters
+    ----------
+    dicti : dict
+       diccionario en el que se recopilo toda la informacion
+       importante de los snapshots para las simulaciones nuevas.
+    dicti_old : dict
+       diccionario en el que se recopilo toda la informacion
+       importante de los snpashots para las simulaciones viejas.
+    gal : dict
+       diccionario en el que se tienen las galaxias que 
+       deseamos analizar.   
+    """
+    snapshots_new = extract_qtt(dicti,'snapshots')
+    h0_gas_new = extract_qtt(dicti,'h_gas')
+    h0_star_new = extract_qtt(dicti,'h_star')
+    N_star_new = extract_qtt(dicti,'Nstar')
+    Vmax_new = extract_qtt(dicti,'Vmax')
+    
+    
+    snapshots_old = extract_qtt(dicti_old,'snapshots')
+    h0_gas_old = extract_qtt(dicti_old,'h_gas')
+    h0_star_old = extract_qtt(dicti_old,'h_star')
+    N_star_old = extract_qtt(dicti_old,'Nstar')
+    Vmax_old = extract_qtt(dicti_old,'Vmax')
+    
+   
+    plt.figure(figsize=(12,4))
+    
+    plt.subplot(131)
+    
+    if gal in ['G0','G1']:
+        plt.hist(h0_gas_new,bins=5,color='green')
+        plt.title(gal + '  h0 GAS')
+        x = h0_gas_old
+        y = np.array([50,50,50])
+        plt.plot(x,y, "ok")
+ 
+    if gal == 'G2':
+        h0_gas_new = extract_qtt(super_dict['G2'],'h_gas')
+        plt.hist(h0_gas_new,bins=20,color='green',range=(0,20))
+        plt.title(gal + '  h0 GAS')
+    if gal == 'G3':
+        h0_gas_new = extract_qtt(super_dict['G3'],'h_gas')
+        plt.hist(h0_gas_new,bins=20,color='green',range=(0,20))
+        plt.title(gal + '  h0 GAS')
+    
+    plt.subplot(132)
+  
+    if gal in ['G0','G1','G2']:
+        plt.hist(h0_star_new,bins=5,color='orange')
+        plt.title(gal + '  h0 STARS')
+    if gal in ['G0','G1']:
+        x = h0_star_old
+        y = np.array([50,50,50])
+        plt.plot(x,y,"ok")
+    
+    if gal == "G3":
+        h0_star_new = extract_qtt(super_dict['G3'],'h_star')
+        plt.hist(h0_star_new,bins=20,color='green',range=(0,20)) 
+        plt.title(gal + '  h0 STARS')
+    
+    plt.subplot(133)
+    plt.hist(Vmax_new,bins=5,color='red')
+    plt.title(gal + '  Max Velocity')
+    
+    if gal == "G3":
+        plt.xlim((170,190)) 
+   
     if gal in ['G0','G1']:
         x = Vmax_old
         y = np.array([50,50,50])
